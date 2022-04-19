d866320a1c8aa39b52e8de4fd7680cd372f976a2
shigeya-t <st_github@namio.jp>
2022-04-19 19:44:44 +0900

    Create sysdig-gcloud.tf

---
+++ sysdig-gcloud.tf
@@ -0,0 +1,18 @@
+variable "sysdig_api_token" {}
+
+provider "google" {
+  project = "sysdig"
+  region = "asia-northeast1"
+}
+
+provider "google-beta" {
+  project = "sysdig"
+  region = "asia-northeast1"
+}
+
+module "secure-for-cloud_example_single-project" {
+  source = "sysdiglabs/secure-for-cloud/google//examples/single-project"
+
+  sysdig_secure_endpoint = "https://app.us4.sysdig.com"
+  sysdig_secure_api_token = var.sysdig_api_token
+}
